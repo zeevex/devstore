4320e255ad980b282360896292feaee78fd31f83
Sudish Joseph <sudish@zeevex.com>
2012-06-04 17:29:58 -0400

    Use Host header for determining hostname.

--- src/devstore/models/hostname.clj
+++ src/devstore/models/hostname.clj
@@ -3,12 +3,23 @@
                   [request :as req]
                   [session :as session]]))
 
-(def ^:private default-hostname
-  (str "localhost:" (get (System/getenv) "PORT" "8080")))
+(defn- init!
+  []
+  true)
+
+(defn- request-hostname
+  "Hostname from the request's Host header."
+  [& [request]]
+  (get-in (req/ring-request) [:headers "host"]))
+
+(defn- request-scheme
+  "Scheme for the request."
+  [& [request]]
+  (:scheme (req/ring-request)))
 
 (defn current-hostname
   []
-  (session/get :hostname default-hostname))
+  (session/get :hostname (request-hostname)))
 
 (defn set-current-hostname
   [hostname]
@@ -16,27 +27,11 @@
     (session/remove! :hostname)
     (session/put!    :hostname hostname)))
 
-(defn our-public-url
-  "The public URL for our service."
-  []
-  (str "http://" (current-hostname) "/"))
-
 (defn our-host-url
   "The host URL of the current request."
   []
-  (let [request (req/ring-request)]
-    (str (name (:scheme request)) "://"
-         (get-in request [:headers "host"]))))
-
-(defn our-url
-  "The URL of the current request."
-  []
-  (let [request (req/ring-request)]
-    (str (name (:scheme request)) "://"
-         (get-in request [:headers "host"])
-         (request :uri)
-         (and (:query-string request)
-              (str "?" (:query-string request))))))
+  (str (request-scheme) "://"
+       (request-hostname)))
 
 (defn return-url-for
   "The return url for OFFER under ACTION."
@@ -48,5 +43,5 @@
 (defn notify-url-for
   "The notify return (IPN) url for OFFER."
   [offer]
-  (str (our-public-url)
+  (str (our-host-url)
        (core/url-for "/notify/ipn/:id" {:id (:id offer)})))
